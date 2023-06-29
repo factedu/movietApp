9c5a038fe1d9ddcde181e2e0dbada14c4e4f0a53
Ravi S Singh <factedu@gmail.com>
2023-06-29 15:57:39 +0530

    Splach scree android

---
+++ android/app/src/main/java/com/moviet/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.moviet;
+
+import android.os.Bundle;
+import android.content.Intent;
+import com.facebook.react.ReactActivity;
+
+public class SplashActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
